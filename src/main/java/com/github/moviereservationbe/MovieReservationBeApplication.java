2b9882171d7c76b55d5a9e9af0d60cb03f402250
soheeparklee <soheeparklee@gmail.com>
2024-04-17 00:59:37 +0100

    springboot init

---
+++ src/main/java/com/github/moviereservationbe/MovieReservationBeApplication.java
@@ -0,0 +1,13 @@
+package com.github.moviereservationbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReservationBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieReservationBeApplication.class, args);
+    }
+
+}
