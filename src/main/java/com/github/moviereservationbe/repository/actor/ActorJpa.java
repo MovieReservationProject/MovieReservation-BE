a3fa1cfe0aee189a1f1c2e8a4d72f4e54a64800c
soheeparklee <soheeparklee@gmail.com>
2024-04-19 12:04:49 +0100

    feat: movie, movieActor, actor JPA

---
+++ src/main/java/com/github/moviereservationbe/repository/actor/ActorJpa.java
@@ -0,0 +1,6 @@
+package com.github.moviereservationbe.repository.actor;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActorJpa extends JpaRepository<Actor, Integer> {
+}
---
+++ src/main/java/com/github/moviereservationbe/repository/movie/MovieJpa.java
@@ -0,0 +1,6 @@
+package com.github.moviereservationbe.repository.movie;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieJpa extends JpaRepository<Movie, Integer> {
+}
