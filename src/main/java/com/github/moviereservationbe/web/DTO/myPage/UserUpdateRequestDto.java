26e645ddb437135c8c52246a4b54eb334d5394df
soheeparklee <soheeparklee@gmail.com>
2024-04-25 19:17:12 +0200

    user update bday format

--- src/main/java/com/github/moviereservationbe/web/DTO/myPage/UserUpdateRequestDto.java
+++ src/main/java/com/github/moviereservationbe/web/DTO/myPage/UserUpdateRequestDto.java
@@ -1,5 +1,6 @@
 package com.github.moviereservationbe.web.DTO.myPage;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
@@ -22,6 +23,7 @@ public class UserUpdateRequestDto {
     private String myId;
     @JsonProperty("password")
     private String password;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     @JsonProperty("birthday")
     private Date birthday;
     @JsonProperty("phone-number")
