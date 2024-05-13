2024a6ca9fd45e9706f4853fbd33e644b7f4b0ad
soheeparklee <soheeparklee@gmail.com>
2024-05-13 21:07:02 +0100

    feat: Email Certification Config

--- src/main/java/com/github/moviereservationbe/config/redis/EmailCertificationConfig.java
+++ src/main/java/com/github/moviereservationbe/config/redis/EmailCertificationConfig.java
@@ -1,4 +1,4 @@
-package com.github.moviereservationbe.config;
+package com.github.moviereservationbe.config.redis;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
