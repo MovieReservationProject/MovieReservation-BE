925a1a587200ec1055a6849a9a25b32a6f5fcb2e
soheeparklee <soheeparklee@gmail.com>
2024-05-16 17:46:30 +0100

    feat: socialLoginController

--- src/main/java/com/github/moviereservationbe/service/service/KakaoUserService.java
+++ src/main/java/com/github/moviereservationbe/service/service/KakaoUserService.java
@@ -1,9 +1,13 @@
 package com.github.moviereservationbe.service.service;
 
+import com.github.moviereservationbe.web.DTO.auth.SocialUserInfoDto;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class KakaoUserService {
+    public SocialUserInfoDto kakaoLogin(String code, HttpServletResponse response) {
+    }
 }
--- src/main/java/com/github/moviereservationbe/web/controller/SocialLoginController.java
+++ src/main/java/com/github/moviereservationbe/web/controller/SocialLoginController.java
@@ -1,7 +1,11 @@
 package com.github.moviereservationbe.web.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.github.moviereservationbe.service.service.KakaoUserService;
+import com.github.moviereservationbe.web.DTO.auth.SocialUserInfoDto;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SocialLoginController {
     private final KakaoUserService kakaoUserService;
 
-    public
+    public SocialUserInfoDto kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException{
+        return kakaoUserService.kakaoLogin(code, response);
+    }
 }
